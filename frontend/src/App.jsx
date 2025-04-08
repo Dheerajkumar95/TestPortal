5a71e52ffa075751a6eba19732951805289ca415
unknown <sidharthsingh8196@gmail.com>
2025-04-08 13:08:02 +0530

    SignUp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import "./css/style.css";
 import { Toaster } from "react-hot-toast";
 import  HomePage from "./components/HomePage"
 import LoginPage from './components/LoginPage';
+import SignUpPage from './components/SignUpPAge';
 
 function App() {
   return (
     <>
       <HomePage/>
       <LoginPage/>
+      <SignUpPage/>
       <Toaster /> 
     </>
   )
---
+++ frontend/src/components/SignUpPage.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export class SignUpPage extends Component {
+  render() {
+    return (
+
+      <div>SignUpPage</div>
+    )
+  }
+}
+
+export default SignUpPage
