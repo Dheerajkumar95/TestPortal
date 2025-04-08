2e9c9c74b7466738ca0d73abb3dde5091ea7260c
unknown <sidharthsingh8196@gmail.com>
2025-04-08 12:53:59 +0530

    Login page Created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,13 @@ import './App.css'
 import "./css/style.css";
 import { Toaster } from "react-hot-toast";
 import  HomePage from "./components/HomePage"
+import LoginPage from './components/LoginPage';
+
 function App() {
   return (
     <>
       <HomePage/>
+      <LoginPage/>
       <Toaster /> 
     </>
   )
